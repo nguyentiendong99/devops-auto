84ebfde2e7e2418d6823ae468216d64772317343
nguyentiendong99 <dong190699@gmail.com>
2022-07-02 22:00:03 +0700

    first commit

---
+++ src/main/java/com/example/devopsauto/DevopsAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopsauto/DevopsAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
